ffb68337a1f6b2d3070c56f1ad25609c1a40d734
gustavocadev <gustavocadev@gmail.com>
2025-02-01 18:08:53 -0500

    Refactor blog index to include CardBlog components for enhanced layout and content display

--- src/routes/blog/index.tsx
+++ src/routes/blog/index.tsx
@@ -1,14 +1,22 @@
 import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
+import { CardBlog } from '~/components/card-blog';
 
 export default component$(() => {
   return (
-    <main class="px-[45px] lg:px-[112px]">
-      <section class="h-full">
-        <article class="grid place-items-center py-[112px] text-center">
-          <h5 class="text-4xl text-semibold">En desarrollo... ✏️</h5>
-        </article>
-      </section>
+    <main class="px-[45px] lg:px-[112px] ">
+      <div class="lg:py-24 lg:px-32 space-y-8">
+        <h3 class="text-4xl font-bold">Blog</h3>
+        <div class="grid grid-cols-1 lg:grid-cols-4 gap-8 lg:gap-18">
+          <CardBlog />
+          <CardBlog />
+
+          <CardBlog />
+          <CardBlog />
+
+          <CardBlog />
+        </div>
+      </div>
     </main>
   );
 });
